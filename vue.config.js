262d739feeaff305b94481e697925db23951d339
johnweb <john@qq.com>
2019-06-25 10:31:01 +0800

    fix:暂没改动

--- vue.config.js
+++ vue.config.js
@@ -24,8 +24,8 @@ module.exports = {
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://192.168.12.31:8199', //参考   https://cli.vuejs.org/zh/guide/mode-and-env.html
-        // target: 'http://47.52.16.236:8199',
+        // target: 'http://192.168.12.31:8199', //参考   https://cli.vuejs.org/zh/guide/mode-and-env.html
+        target: 'http://47.52.16.236:8199',
         //target: 'http://192.168.27.245:8199',//lanjie
         changeOrigin: true,
         pathRewrite: {
