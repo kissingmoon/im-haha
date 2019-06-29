c935b902b491637959a24e0f09eb48f41ba9a444
masteronelight <masteronelight@gmail.com>
2019-06-29 17:25:19 +0800

    fix：修改9002登录状态

--- vue.config.js
+++ vue.config.js
@@ -24,9 +24,9 @@ module.exports = {
     port: 8080,
     proxy: {
       '/api': {
-        // target: 'http://47.52.16.236:8199', //参考   https://cli.vuejs.org/zh/guide/mode-and-env.html
+        target: 'http://47.52.16.236:8199', //test 参考   https://cli.vuejs.org/zh/guide/mode-and-env.html
         // target: 'http://192.168.12.33:8199', 
-        target: 'http://192.168.27.245:8199',
+        // target: 'http://192.168.27.245:8199',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/api' //代理的路径
